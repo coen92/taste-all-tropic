4f4b296b4578164095e2fd8797abef4da3dff055
coen92 <piotrkapica92@gmail.com>
2025-08-07 22:24:24 +0200

    TAT-02: initialized order-management-service as inner-module

---
+++ order-management-service/src/test/java/com/coen92/tat/ordermanagementservice/OrderManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coen92.tat.ordermanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
