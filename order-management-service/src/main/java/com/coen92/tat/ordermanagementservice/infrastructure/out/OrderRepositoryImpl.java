7108376e4a98f10ddd4b3a343d8e289c9e5209cd
coen92 <piotrkapica92@gmail.com>
2025-08-11 20:10:18 +0200

    TAT-02: setup database with spring data jdbc connection with flyway migration

--- order-management-service/src/main/java/com/coen92/tat/ordermanagementservice/infrastructure/out/OrderRepositoryImpl.java
+++ order-management-service/src/main/java/com/coen92/tat/ordermanagementservice/infrastructure/out/OrderRepositoryImpl.java
@@ -1,20 +1,33 @@
 package com.coen92.tat.ordermanagementservice.infrastructure.out;
 
 import com.coen92.tat.ordermanagementservice.domain.customerorder.CustomerOrder;
-import com.coen92.tat.ordermanagementservice.domain.customerorder.OrderId;
 import com.coen92.tat.ordermanagementservice.domain.customerorder.OrderRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.sql.Timestamp;
+import java.time.Clock;
 
 @Repository
+@RequiredArgsConstructor
 public class OrderRepositoryImpl implements OrderRepository {
-    // temporary in-memory storage (later full database connection introduced)
-    private Map<OrderId, CustomerOrder> orders = new HashMap<>();
+    private final OrderJdbcRepository repository;
+    private final Clock clock;
 
     @Override
     public void save(CustomerOrder order) {
-        orders.put(order.getOrderId(), order);
+        repository.save(toEntity(order));
+    }
+
+    private CustomerOrderEntity toEntity(CustomerOrder order) {
+        return CustomerOrderEntity.builder()
+                .orderId(order.getOrderId().getValue())
+                .createdAt(Timestamp.from(clock.instant()))
+                .updatedAt(Timestamp.from(clock.instant()))
+                .version(0)
+                .payload(new CustomerOrderEntity.CustomerOrderPayload(
+                        // todo: fill with actual fields
+                ))
+                .build();
     }
 }
